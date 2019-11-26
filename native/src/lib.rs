71f3de317ee116e0bc9a379500b0f40db6a23186
Bennett Hardwick <me@bennetthardwick.com>
2019-11-26 23:41:39 +1000

    Add bad factorial

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,50 @@
-#[macro_use]
 extern crate neon;
+extern crate rayon;
 
 use neon::prelude::*;
+use rayon::prelude::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+struct FactorialTask {
+    // Factorial number to sum up to
+    n: usize,
 }
 
-register_module!(mut cx, {
-    cx.export_function("hello", hello)
-});
+impl Task for FactorialTask {
+    type Output = u128;
+    type Error = String;
+    type JsEvent = JsNumber;
+
+    fn perform(&self) -> Result<Self::Output, Self::Error> {
+        let result = (1..self.n + 1)
+            .into_par_iter()
+            .map(|x| x as u128)
+            .reduce_with(std::ops::Mul::mul);
+
+        if let Some(value) = result {
+            Ok(value)
+        } else {
+            Err(String::from("Something went wrong"))
+        }
+    }
+
+    fn complete(
+        self,
+        mut cx: TaskContext,
+        result: Result<Self::Output, Self::Error>,
+    ) -> JsResult<Self::JsEvent> {
+        Ok(cx.number(result.unwrap() as f64))
+    }
+}
+
+fn factorial(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    let n = cx.argument::<JsNumber>(0)?.value() as usize;
+
+    let task = FactorialTask { n };
+
+    let callback = cx.argument::<JsFunction>(1)?;
+    task.schedule(callback);
+
+    Ok(cx.undefined())
+}
+
+register_module!(mut cx, { cx.export_function("factorial", factorial) });
